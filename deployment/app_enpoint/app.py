4a3944984005e67f78ca3810ef2762b0ca6b9b0d
Oudelet Kevin <kevin.oudelet@gmail.com>
2024-01-21 17:40:22 +0100

    formulaire ok, testing pickle/joblib

--- deployment/app_enpoint/app.py
+++ deployment/app_enpoint/app.py
@@ -1,12 +1,49 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, request, jsonify
+
+# import pickle
+import joblib
+
 
 app = Flask(__name__)
 
-@app.route("/predict")
-def hello():
+@app.route('/predict', methods=['POST'])
+def predict_topics():
+    try:
+        # Get content from the POST request
+        content = request.form['content']
 
-    return "Hello World!"
+        # Call your topics prediction function
+        predicted_topics = custom_knn(content)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        # save the model to disk
+        # pickle first, test joblib for knn
+        filename = 'finalized_model.sav'
+        pickle.dump(model, open(filename, 'wb'))
+
+        # Export your model to a file
+        with open('my_model.pkl', 'wb') as f:
+            pickle.dump(model, f)
+
+        # save the model to disk
+        filename = 'finalized_model.sav'
+        joblib.dump(model, filename)
+
+        # some time later...
+
+        # load the model from disk
+        loaded_model = joblib.load(filename)
+        result = loaded_model.score(X_test, Y_test)
+        print(result)
+
+        # some time later...
+
+        # load the model from disk
+        loaded_model = pickle.load(open(filename, 'rb'))
+        result = loaded_model.score(X_test, Y_test)
+
+        # Return the result as JSON
+        return jsonify({'result': predicted_topics})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
